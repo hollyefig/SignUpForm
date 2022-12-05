d20783e2da8c8154e4b9d4a44265fa8ed00bad65
“Holly <hollyefig@gmail.com>
2022-12-05 10:36:33 -0500

    added on entire site

---
+++ script.js
@@ -0,0 +1,26 @@
+const input = document.querySelectorAll("input");
+const label = document.querySelectorAll("label");
+const errorMsg = document.querySelectorAll(".error");
+let firstEntry = [false, false, false, false, false, false];
+
+const inputEntered = (e) => {
+  for (let i = 0; i < label.length; i++) {
+    if (label[i].htmlFor === e.id) {
+      firstEntry[i] = true;
+      if (e.value !== "") {
+        label[i].classList.add("labelSmall");
+        errorMsg[i].textContent = "";
+      } else {
+        label[i].classList.remove("labelSmall");
+        errorMsg[i].textContent = "* Required Field";
+      }
+    }
+  }
+  const pw1 = input[4].value;
+  const pw2 = input[5].value;
+  if (firstEntry[5] === true) {
+    pw1 !== pw2
+      ? (errorMsg[5].textContent = "* Passwords do not match")
+      : (errorMsg[5].textContent = "✔️");
+  }
+};
